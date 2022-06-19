6d0a3c2f32ed7e77c2ee44a94ac9e2599c88ff01
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-19 22:24:21 +0530

    [gurpreet] UC3_Appending

--- LinkedList/LinkedList/LinkedList.cs
+++ LinkedList/LinkedList/LinkedList.cs
@@ -29,27 +29,44 @@ namespace LinkedList
             }
 
         }
-        internal void Display()
-        {
-            Node temp = Head;
-            if (temp == null)
-                Console.WriteLine("Linked list is empty");
-            else
-                Console.WriteLine("Linked list");
-            while (temp != null)
-            {
-                Console.WriteLine(temp.data);
-                if (temp.next != null)
-                    Console.WriteLine(">");
-                temp = temp.next;
-            }
-        }
+        //internal void Display()
+        //{
+        //    Node temp = Head;
+        //    if (temp == null)
+        //        Console.WriteLine("Linked list is empty");
+        //    else
+        //        Console.WriteLine("Linked list");
+        //    while (temp != null)
+        //    {
+        //        Console.WriteLine(temp.data);
+        //        if (temp.next != null)
+        //            Console.WriteLine(">");
+        //        temp = temp.next;
+        //    }
+        //}
         public void AddatFirst(int element)
         {
             Node newnode = new Node(element);
             newnode.next = Head;
             Head = newnode;
 
+
+
+        }
+        public void append(int newdata)
+        {
+            Node new_node = new Node(newdata);
+            if (Head == null)
+            {
+                Head = new Node(newdata);
+                return;
+            }
+            new_node.next = null;
+            Node last = Head;
+            while (last.next != null)
+                last = last.next;
+            last.next = new_node;
+            return;
 
 
         }
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -11,10 +11,14 @@
             //linked.AddNode(node2);
             Node node3 = new Node(30);
             //linked.AddNode(node3);
-            linked.Display();
-            linked.AddatFirst(70);
-            linked.AddatFirst(30);
-            linked.AddatFirst(56);
+            //linked.Display();
+            //linked.AddatFirst(70);
+            //linked.AddatFirst(30);
+            //linked.AddatFirst(56);
+            linked.append(56);
+            linked.append(30);
+            linked.append(70);
+
 
             linked.printList();
 
