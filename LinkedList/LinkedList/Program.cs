3d1e3ece1ff72053f070b70eee3a4b52ba024624
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-19 22:09:41 +0530

    [gurpreet] UC1_SimpleLinkedList

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LinkedList
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to linkedlist");
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+
+        }
+    }
+}
+
